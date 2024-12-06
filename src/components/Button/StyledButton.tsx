7cb04c6f66ffeca8de7b7177715ce1b21b095749
Carol B <carolb@bu.edu>
2024-12-06 18:27:38 -0500

    Created reusable styled button

---
+++ src/components/Button/StyledButton.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+
+type StyledButtonProps = {
+  btnText: string;
+  handleClick: () => void;
+  disabled?: boolean;
+};
+export default function StyledButton({
+  btnText,
+  handleClick,
+  disabled,
+}: StyledButtonProps) {
+  return (
+    <Button
+      className="text-bold mb-14 bg-yellow-400 py-5 text-lg uppercase text-blue-950"
+      onClick={handleClick}
+      disabled={disabled ?? false}
+    >
+      {btnText}
+    </Button>
+  );
+}
